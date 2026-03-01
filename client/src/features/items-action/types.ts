a37222458bd76c738db5a477eba9b1348eac7a67
Joseph Murphy <murphyjoseph@users.noreply.github.com>
2026-03-01 11:24:47 -0600

    chore: clean slate

--- client/src/features/items-action/types.ts
+++
@@ -1,8 +0,0 @@
-export interface Item {
-  id: number;
-  title: string;
-  description: string | null;
-  priority: number;
-  status: string;
-  createdAt: string;
-}
--- client/src/features/items-form/controllers/create-item-form.tsx
+++
@@ -1,7 +0,0 @@
-import { useCreateItemForm } from '../hooks/use-create-item-form';
-import { CreateItemFormView } from '../views/create-item-form-view';
-
-export function CreateItemForm() {
-  const form = useCreateItemForm();
-  return <CreateItemFormView form={form} />;
-}
